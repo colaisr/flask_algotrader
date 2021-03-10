ee4524f40c0111563ae96afdd5f914291812906e
colakamornik <cola.isr@gmail.com>
2021-03-10 03:00:36 +0300

    indicator ofline online is set

--- app/static/assets/scripts/traderstation.js
+++ app/static/assets/scripts/traderstation.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 paint_pnl()
-
+set_connection_state()
 setTimeout(function(){
    window.location.reload(1);
 }, 30000);
 
 })
+function set_connection_state(){
+el=$("#seconds_passed").val()
+if(el>30){
+el=$("#connection_state").addClass('badge-danger').removeClass('badge-success');
+el=$("#connection_state").text("Offline");
+}else{
+el=$("#connection_state").addClass('badge-success').removeClass('badge-danger');
+el=$("#connection_state").text("Online");
+}
+
+
+}
 function paint_pnl() {
 box_pnl=$(".box_pnl");
 val_pnl=$(".val_pnl").html();
--- app/userview/views.py
+++ app/userview/views.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from flask import (
     Blueprint,
@@ -69,7 +70,11 @@ def traderstationstate():
                 v['profit_class'] = 'text-danger'
                 v['profit_progress_colour'] = 'bg-danger'
                 v['profit_progress_percent'] = abs(profit / 10 * 100)
-    return render_template('userview/traderstationstate.html',open_positions=open_positions,open_orders=open_orders, user=current_user,report=report,margin_used=use_margin, form=None)
+
+        report_time=report.report_time
+        current_time=datetime.now()
+        seconds_since_last_report=(current_time-report_time).total_seconds()
+    return render_template('userview/traderstationstate.html',seconds_passed=seconds_since_last_report,open_positions=open_positions,open_orders=open_orders, user=current_user,report=report,margin_used=use_margin, form=None)
 
 @userview.route('closedpositions', methods=['GET', 'POST'])
 @login_required
