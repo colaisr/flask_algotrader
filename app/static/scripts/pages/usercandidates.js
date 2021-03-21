a63cdd1ab14ee3a09f1948dcbbe661008d0199be
colakamornik <cola.isr@gmail.com>
2021-03-21 19:42:25 +0300

    model 50 works

--- app/static/scripts/pages/usercandidates.js
+++ app/static/scripts/pages/usercandidates.js
@@ -12,7 +12,11 @@
             }
         });
 
-//        $('.modal-dialog').parent().on('show.bs.modal', function(e){        $(e.relatedTarget.attributes['data-target'].value).appendTo('body'); })
+
+
+
+
+
 })
 
 $('.btn_edit').on('click', function() {
