c9f79eeeff6833db1a94a227da2fcaec236d0e17
colakamornik <cola.isr@gmail.com>
2021-03-24 19:48:15 +0300

    old databases removed

--- update_database.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-
-python3 manage.py db migrate -m "added report fields"
-python3 manage.py db upgrade
-
-
-#should be done on the first time only
-python3 manage.py db init --multidb
-python3 manage.py db stamp head
