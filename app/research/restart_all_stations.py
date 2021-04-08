27d8b9402ab172961ae51ff8a39093416d4c5002
colakamornik <cola.isr@gmail.com>
2021-04-08 23:00:28 +0300

    file for restart job added

--- app/research/restart_all_stations.py
+++ app/research/restart_all_stations.py
@@ -3,5 +3,8 @@ from urllib.request import urlopen
 server_url="http://colak.eu.pythonanywhere.com/"
 
 url =server_url+"/connections/restart_all_stations"
-response = urllib.request.urlopen(url)
+
+data = urllib.parse.urlencode({"requester": 'admin spider'})
+data = data.encode('ascii')
+response = urllib.request.urlopen(url,data)
 
