6ec06fe666a0d36d0c2d836e0efe3255d4ba52d8
colakamornik <cola.isr@gmail.com>
2021-07-27 10:48:44 +0300

    api connected report metrics

--- app/models/clients.py
+++ app/models/clients.py
@@ -40,19 +40,21 @@ class Report(db.Model):
             db.session.add(self)
         else:
             report.report_time=self.report_time
-            report.net_liquidation = self.net_liquidation
-            report.remaining_sma_with_safety = self.remaining_sma_with_safety
-            report.excess_liquidity = self.excess_liquidity
-            report.remaining_trades = self.remaining_trades
-            report.all_positions_value = self.all_positions_value
-            report.open_positions_json = self.open_positions_json
-            report.open_orders_json = self.open_orders_json
-            report.candidates_live_json=self.candidates_live_json
-            report.dailyPnl = self.dailyPnl
-            report.last_worker_execution = self.last_worker_execution
-            report.market_time = self.market_time
             report.market_state = self.market_state
             report.started_time = self.started_time
             report.api_connected = self.api_connected
+            if report.api_connected:
+                report.net_liquidation = self.net_liquidation
+                report.remaining_sma_with_safety = self.remaining_sma_with_safety
+                report.excess_liquidity = self.excess_liquidity
+                report.remaining_trades = self.remaining_trades
+                report.all_positions_value = self.all_positions_value
+                report.open_positions_json = self.open_positions_json
+                report.open_orders_json = self.open_orders_json
+                report.candidates_live_json=self.candidates_live_json
+                report.dailyPnl = self.dailyPnl
+                report.last_worker_execution = self.last_worker_execution
+                report.market_time = self.market_time
+
 
         db.session.commit()
