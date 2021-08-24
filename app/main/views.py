f50b50bb0f1abe94d3de67c11c227c73e4b8e1a1
lilia <liliana.isr@gmail.com>
2021-08-24 11:52:41 +0300

    utc to local time

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,8 @@ from flask_login import current_user
 from sqlalchemy import text
 from werkzeug.utils import redirect
 
+import app.generalutils as general
+
 from app.models import EditableHTML, User, TickerData, Position, Report, LastUpdateSpyderData
 
 from app import db
@@ -20,8 +22,11 @@ def index():
         query_text = "select a.* from Tickersdata a join (  select Tickersdata.`ticker`, max(Tickersdata.`updated_server_time`) as updated_server_time  from Tickersdata group by Tickersdata.`ticker`) b on b.`ticker`=a.`ticker` and b.`updated_server_time`=a.`updated_server_time`"
         uniq_tickers_data = db.session.query(TickerData).from_statement(text(query_text)).all()
         system_status['tickers_tracked'] = len(uniq_tickers_data)
+
         last_update_date = db.session.query(LastUpdateSpyderData.last_update_date).scalar()
-        system_status['last_update_date'] = last_update_date.strftime("%d-%b-%Y %H:%M")
+        central = general.utc_datetime_to_local(last_update_date)
+        system_status['last_update_date'] = central.strftime("%d-%b-%Y %H:%M")
+
         system_status['users_registered'] = len(Report.query.all())
         system_status['lost_positions'] = len(
             Position.query.filter(Position.profit <= 0, Position.last_exec_side == 'SLD').all())
