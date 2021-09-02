10991d3c906626ae36fd141e59bc83076b8ebd13
colakamornik <cola.isr@gmail.com>
2021-09-02 12:57:09 +0300

    telegram signal partial

--- app/connections/views.py
+++ app/connections/views.py
@@ -175,7 +175,7 @@ def logreport():
         report.started_time = datetime.fromisoformat(request_data["started_time"])
         report.market_data_error = request_data["market_data_error"]
         report.update_report()
-        if report.market_state!="Open":
+        if report.market_state=="Open":
             check_for_signals(report.candidates_live_json)
 
         return "Report snapshot stored at server"
