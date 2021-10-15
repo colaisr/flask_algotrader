6b638c61c1a50bd4e6002e5d25051c59187f91d1
colakamornik <cola.isr@gmail.com>
2021-10-15 08:31:32 +0300

    history graph moved to Yahoo history data

--- app/static/scripts/pages/ticker_info.js
+++ app/static/scripts/pages/ticker_info.js
@@ -1,16 +1,17 @@
 
 ticker=$( "#ticker" )[0].value;
 
-url='https://financialmodelingprep.com/api/v3/historical-price-full/'+ticker+'?serietype=line&apikey=f6003a61d13c32709e458a1e6c7df0b0'
+//url='https://financialmodelingprep.com/api/v3/historical-price-full/'+ticker+'?serietype=line&apikey=f6003a61d13c32709e458a1e6c7df0b0'
+url='https://colak.eu.pythonanywhere.com/research/get_complete_graph_for_ticker/'+ticker
 $.getJSON(url, function(data) {
     data=data['historical']
     var arr = [];
     for (d of data)
     {
-        parsed_d=Date.parse(d["date"]);
-        arr.push( [parsed_d , d["close"] ]);
+        parsed_d=Date.parse(d["Date"]);
+        arr.push( [parsed_d , d["Close"] ]);
     }
-    rev_main=arr.reverse()
+    rev_main=arr
     Highcharts.stockChart('container', {
         rangeSelector: {
             selected: 1
