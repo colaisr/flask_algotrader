66f595059e7c72878407202cda97ff1f8207a638
LilianaIL <liliana.isr@gmail.com>
2021-10-22 18:59:23 +0300

    stop reload traderstation when modal opened

--- app/static/scripts/pages/traderstation.js
+++ app/static/scripts/pages/traderstation.js
@@ -1,8 +1,12 @@
+var interval;
+
 $(document).ready(function(){
     //paint_pnl()
 //    setTimeout(function(){
 //       window.location.reload(1);
 //    }, 30000);
+
+    interval_start();
 
     var emotion_settings = parseInt($('#user-emotion').val());
     var main_snp = [];
@@ -11,13 +15,22 @@ $(document).ready(function(){
     fill_emotion_and_snp_graphs(emotion_settings, false, main_snp, main_emotion);
 
     $('#user_emotion_box').click(function(){
+        clearInterval(interval);
         $(".emotion-modal").show();
     });
 
     $('.emotion-modal-close').click(function(){
         $(".emotion-modal").hide();
+//        interval_start();
+        window.location.reload(1);
     });
 })
+
+function interval_start(){
+    interval = setInterval(function(){
+       window.location.reload(1);
+    }, 30000);
+}
 
 function fill_graph(){
     $("#sectors_modal").modal("show");
