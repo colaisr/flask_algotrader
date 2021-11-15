7906666c595d42a1fd606c535bdb0afa21866fa2
LilianaIL <liliana.isr@gmail.com>
2021-11-15 21:38:51 +0300

    db prop names

--- app/models/fgi_score.py
+++ app/models/fgi_score.py
@@ -4,9 +4,11 @@ from .. import db
 class Fgi_score(db.Model):
     __tablename__ = 'Fgi_Scores'
     id = db.Column('id', db.Integer, primary_key=True)
-    score_time = db.Column('score_time_cnn', db.DateTime)
+    score_time = db.Column('score_time', db.DateTime)
     fgi_value = db.Column('fgi_value_cnn', db.Integer)
     fgi_text = db.Column('fgi_text_cnn', db.String)
+    updated_cnn = db.Column('updated', db.String)
+
 
     def add_score(self):
         db.session.add(self)
