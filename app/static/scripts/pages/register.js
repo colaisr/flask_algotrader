4bf5827d53da92a786a6299f5ff07e8a543ad822
LilianaIL <liliana.isr@gmail.com>
2021-11-22 14:17:37 +0300

    google register

--- app/static/scripts/pages/register.js
+++ app/static/scripts/pages/register.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
     if($('#terms_agree').is(':checked')){
         $('#submit').removeClass('disabled');
+        $('.btn-google').removeClass('disabled');
     }
     else{
         $('#submit').addClass('disabled');
+        $('.btn-google').addClass('disabled');
     }
 
     $('#terms_agree').click(function(){
         if ($(this).is(':checked')){
             $('#submit').removeClass('disabled');
+            $('.btn-google').removeClass('disabled');
         } else {
             $('#submit').addClass('disabled');
+            $('.btn-google').addClass('disabled');
         }
     });
 
