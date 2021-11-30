bca2680df03db556e191d9fe080cc4c1b3f537b2
colakamornik <cola.isr@gmail.com>
2021-11-30 21:48:05 +0300

    no duplication - cleaning list

--- app/static/scripts/pages/base_new.js
+++ app/static/scripts/pages/base_new.js
@@ -9,10 +9,12 @@ $(".search-input").on("input", function() {
 
 function get_search_results(text_to_search){
 $.getJSON( "https://colak.eu.pythonanywhere.com/data_hub/search_quick/"+text_to_search, function( data ) {
-  var items = [];
+
+  $( ".search-suggested" ).remove();
   $.each( data, function( key, val ) {
-    $( "#search_results" ).after(
-     '<a class="dropdown-item px-card py-2" href="/candidates/info/' + val["symbol"] + '"><div class="d-flex align-items-center"><div class="avatar avatar-l status-online me-2"><img class="rounded-circle" src="assets/img/team/1.jpg" alt="" /></div><div class="flex-1"><h6 class="mb-0 title">'+val["symbol"]+'</h6><p class="fs--2 mb-0 d-flex">'+val["name"]+'</p></div></div></a>'
+
+    $( "#search_results" ).append(
+     '<a class="search-suggested dropdown-item px-card py-2" href="/candidates/info/' + val["symbol"] + '"><div class="d-flex align-items-center"><div class="avatar avatar-l status-online me-2"><img class="rounded-circle" src="assets/img/team/1.jpg" alt="" /></div><div class="flex-1"><h6 class="mb-0 title">'+val["symbol"]+'</h6><p class="fs--2 mb-0 d-flex">'+val["name"]+'</p></div></div></a>'
      );
   });
   b=2
