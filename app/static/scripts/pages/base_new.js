e3a5b6ec240e3f89b668dbf2e079997093af3784
colakamornik <cola.isr@gmail.com>
2021-12-01 11:31:55 +0300

    search sending a default

--- app/static/scripts/pages/base_new.js
+++ app/static/scripts/pages/base_new.js
@@ -2,6 +2,17 @@ $(document).ready(function () {
 
 
 });
+$("#header_search").submit(function(e){
+   active_items=$("#search_results > a.search-suggested.dropdown-item.active.px-card.py-2")
+   if(active_items.length>0){
+   target=active_items[0].href
+   window.location.href = target;
+   }
+   else{
+   //alert( "Handler for .submit() called." );    test only - will do nothing
+   }
+    return false;
+});
 
 $(".search-input").on("input", function() {
    get_search_results($(this).val());
@@ -12,14 +23,19 @@ $.getJSON( "https://colak.eu.pythonanywhere.com/data_hub/search_quick/"+text_to_
 
   $( ".search-suggested" ).remove();
   $.each( data, function( key, val ) {
-
-    $( "#search_results" ).append(
-     '<a class="search-suggested dropdown-item px-card py-2" href="/candidates/info/' + val["symbol"] + '"><div class="d-flex align-items-center"><div class="avatar avatar-l status-online me-2"><img class="rounded-circle" src="assets/img/team/1.jpg" alt="" /></div><div class="flex-1"><h6 class="mb-0 title">'+val["symbol"]+'</h6><p class="fs--2 mb-0 d-flex">'+val["name"]+'</p></div></div></a>'
+    if(key==0)
+    {
+        $( "#search_results" ).append(
+     '<a class="search-suggested dropdown-item active px-card py-2" href="/candidates/info/' + val["symbol"] + '"><div class="d-flex align-items-center"><div class="flex-1"><h6 class="mb-0 title">'+val["symbol"]+'</h6><p class="fs--2 mb-0 d-flex">'+val["name"]+'</p></div></div></a>'
      );
+    }else{
+    $( "#search_results" ).append(
+     '<a class="search-suggested dropdown-item px-card py-2" href="/candidates/info/' + val["symbol"] + '"><div class="d-flex align-items-center"><div class="flex-1"><h6 class="mb-0 title">'+val["symbol"]+'</h6><p class="fs--2 mb-0 d-flex">'+val["name"]+'</p></div></div></a>'
+     );
+     }
   });
-  b=2
-
 });
+//$(".search-suggested")[0].addClass('dropdown-item.active');
 }
 
 
