99c73f6522db22693cdb4e4bfcccd96729373d61
LilianaIL <liliana.isr@gmail.com>
2021-12-08 22:15:39 +0300

    ticker info test

--- app/static/scripts/pages/ticker_info_new.js
+++ app/static/scripts/pages/ticker_info_new.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     get_avg_pe_from_fmp($('.ticker-sector-val').data('sector'));
 //    get_fmp_ticker_data(ticker);
     get_stock_news(10);
-//    get_insider_actions();
+    get_insider_actions();
 //    get_press_relises();
 //    get_fundamentals_summary();
 //    get_fundamentals_feed();
